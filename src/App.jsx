667d3b8be315c15acfb1bc358f3d630be4b18962
Sejal-luhar <sejalluhar2001@gmail.com>
2024-05-08 13:37:54 +0530

    bharat-clock

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AppName from "./components/appname";
+import Line1 from "./components/line1";
+import Line2 from "./components/line2";
+function App(){
+  return <center>
+    <AppName></AppName>
+    <Line1></Line1>
+    <Line2></Line2>
+   </center>
+}
+export default App;
---
+++ src/components/line1.jsx
@@ -0,0 +1,6 @@
+function Line1(){
+  return (<p className="lead">This is the clock that shows the time in Bharat at all times
+
+  </p>);
+}
+export default Line1;
